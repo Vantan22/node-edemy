8f5e0cacd0cb07653764a2577af9f75e150d4a95
Charlie Nguyen <vawntanng@gmail.com>
2024-05-16 13:16:41 +0700

    [WORKING WITH DYNAMIC CONTENT & ADDING TEMPLATING ENGINES] Sharing Data Across Requests & Users

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const http = require("http");
-const routes = require("./routes.js");
-const express = require("express");
+const routes = require('./routes.js');
+const express = require('express');
 const app = express();
-const path = require("path");
-const bodyParser = require("body-parser");
-const adminRouter = require("./routers/admin");
-const shopRouter = require("./routers/shop");
-const rootDir = require("./util/path");
+const path = require('path');
+const bodyParser = require('body-parser');
+const adminData = require('./routers/admin');
+const shopRouter = require('./routers/shop');
+const rootDir = require('./util/path');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootDir, "public")));
-app.use("/admin", adminRouter);
+app.use(express.static(path.join(rootDir, 'public')));
+app.use('/admin', adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 app.listen(3000);
