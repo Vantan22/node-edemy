7e3d4d7f38dbdc8ced5dbeb683161e80f28d1902
Charlie Nguyen <vawntanng@gmail.com>
2024-05-21 08:14:43 +0700

    [SQL INTRODUCTION] Fetching a single product with the "where" condition

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'Vantan22@'
+    password: 'nodecomplete'
 });
 
 module.exports = pool.promise();
