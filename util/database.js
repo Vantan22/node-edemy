d4f3a53cd7ba741afee7878e0063aba4ad2f7def
Charlie Nguyen <vawntanng@gmail.com>
2024-05-21 12:06:18 +0700

    [WORKING WITH NOSQL & USING MONGODB] Fetching a single product with the "where" condition

--- util/database.js
+++ util/database.js
@@ -1,10 +1,28 @@
-const mysql = require('mysql2');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
-});
+let _db;
+const dbUrl = 'mongodb+srv://admin:admin@cluster0.qfsc0hv.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0';
+const mongoConnect = (callback) => {
 
-module.exports = pool.promise();
+  MongoClient.connect(dbUrl
+  ) .then(client => {
+    console.log('Connected!');
+    _db = client.db();
+    callback();
+  })
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
